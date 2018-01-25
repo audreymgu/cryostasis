405971340c4e090350cc8501e01511c60886fd09
Audrey Gu <audreymgu@gmail.com>
2018-01-25 01:53:18 -0500

    Added dynamic inventory management and basic interactivity.

--- Exercise1/InteractiveFiction.cs
+++ Exercise1/InteractiveFiction.cs
@@ -20,6 +20,11 @@ public class InteractiveFiction {
       Location reconditioning = new Location("Reconditioning Room");
       reconditioning.Description = "A room with what looks to be a variety of medical equipment. There are lockers to your left and your right.";
 
+      reconditioning.Examination = "You cautiously look around the room; it appears to be connected on three sides to rooms that appear to have served a similar purpose to the room in which you woke up. While the windows to Cryosleep B and Cryosleep C allow you to peer inside, the window to Cryosleep A is completely frosted over. Checking the lockers on your right reveals an odd looking suit and a thin red card.";
+
+      Location hallway_s = new Location("South Hallway");
+      hallway_s.Description = "A long hallway running from West to East.";
+
       //Location linkages
 
       //cryo_a
@@ -32,10 +37,15 @@ public class InteractiveFiction {
       cryo_c.West = reconditioning;
 
       //reconditioning
+      reconditioning.LockExists = true;
+      reconditioning.North = hallway_s;
+      reconditioning.NorthLocked = true;
+      reconditioning.NorthKey = "redcard";
       reconditioning.South = cryo_b;
       reconditioning.East = cryo_c;
       reconditioning.West = cryo_a;
-
+      reconditioning.WestLocked = true;
+      reconditioning.WestKey = "redcard";
 
       //Set current location
       Location currentLocation;
@@ -46,6 +56,15 @@ public class InteractiveFiction {
       Item widget = new Item("Widget");
       widget.Details = "A widget.";
       inventory.Add(widget);
+
+      //Create room items
+      Item suit = new Item("Suit");
+      suit.Details = "A full-body suit. It gleams an iridescent red under the flourescent lights.";
+      reconditioning.RoomContains.Add(suit);
+
+      Item rcard = new Item("RedCard");
+      rcard.Details = "A small red card. The words EMPLOYEE ACCESS are emblazoned in white lettering across one side.";
+      reconditioning.RoomContains.Add(rcard);
 
       //Game loop
       bool playing = true;
@@ -69,18 +88,20 @@ public class InteractiveFiction {
         //Move to a new location
         if (splitCommands[0].ToLower() == "go") {
           try {
-            if (splitCommands[1].ToLower() == "north" && currentLocation.North != null) {
+            if (splitCommands[1].ToLower() == "north" && currentLocation.North != null && currentLocation.NorthLocked == false) {
               currentLocation.JustArrived = true;
               currentLocation = currentLocation.North;
-            } else if (splitCommands[1].ToLower() == "south" && currentLocation.South != null) {
+            } else if (splitCommands[1].ToLower() == "south" && currentLocation.South != null && currentLocation.SouthLocked == false) {
               currentLocation.JustArrived = true;
               currentLocation = currentLocation.South;
-            } else if (splitCommands[1].ToLower() == "east" && currentLocation.East != null) {
+            } else if (splitCommands[1].ToLower() == "east" && currentLocation.East != null && currentLocation.EastLocked == false) {
               currentLocation.JustArrived = true;
               currentLocation = currentLocation.East;
-            } else if (splitCommands[1].ToLower() == "west" && currentLocation.West != null) {
+            } else if (splitCommands[1].ToLower() == "west" && currentLocation.West != null && currentLocation.WestLocked == false) {
               currentLocation.JustArrived = true;
               currentLocation = currentLocation.West;
+            } else if (currentLocation.LockExists == true) {
+              Console.WriteLine("Locked.");
             } else {
               Console.WriteLine("You tried to go to the " + splitCommands[1] + " and there was nothing to the " + splitCommands[1] + ".");
             }
@@ -91,27 +112,95 @@ public class InteractiveFiction {
           }
         }
 
-        //Check inventory
-        if (splitCommands[0].ToLower() == "inventory") {
-          Console.WriteLine("--- INVENTORY ---");
-          //Loop through everything in the inventory and print out its name
-          foreach(Item item in inventory) {
-            Console.WriteLine("\t" + item.Name);
+        //Unlock a path
+        if (splitCommands[0].ToLower() == "unlock") {
+          try {
+            // Check whether player actually has item
+            bool hasItem = false;
+            foreach(var item in inventory) {
+              if (splitCommands[2].ToLower() == item.Name.ToLower()) {
+                hasItem = true;
+              }
+            }
+            // If so, check if item is the right item
+            if (hasItem) {
+              string unlockStatus = currentLocation.UnlockDaemon(splitCommands[1].ToLower(), splitCommands[2].ToLower());
+              Console.WriteLine(unlockStatus);
+            } else {
+              Console.WriteLine("You do not have " + splitCommands[2].ToLower() + ".");
+            }
+          }
+          catch (IndexOutOfRangeException) {
+            Console.WriteLine("To unlock a path, specify the direction (north, south, east, west) and the item you would like to use.");
           }
         }
 
-        //Examine item
-        if (splitCommands[0].ToLower() == "examine") {
+        //Check inventory
+        if (splitCommands[0].ToLower() == "inventory" || splitCommands[0].ToLower() == "items") {
+          Console.WriteLine("--- INVENTORY ---");
+          //Loop through everything in the inventory and print out its name
+          foreach(Item item in inventory) {
+            Console.WriteLine(item.Name);
+          }
+        }
+
+        // Pick up item
+        if (splitCommands[0].ToLower() == "pick" && splitCommands[1].ToLower() == "up") {
           try {
-            foreach(var item in inventory) {
-              if (splitCommands[1].ToLower() == item.Name.ToLower()) {
-                Console.WriteLine(item.Details);
+            Item match = null;
+            foreach(var item in currentLocation.RoomContains) {
+              if (splitCommands[2].ToLower() == item.Name.ToLower()) {
+                match = item;
               }
             }
+            inventory.Add(match);
+            currentLocation.RoomContains.Remove(match);
+            Console.WriteLine("You picked up the " + match.Name.ToLower() + ".");
+          }
+          catch (IndexOutOfRangeException) {
+            Console.WriteLine("Please choose an item to pick up.");
+          }
+        }
+
+        //Drop item
+        if (splitCommands[0].ToLower() == "drop") {
+          try {
+            Item match = null;
+            foreach(var item in inventory) {
+              if (splitCommands[1].ToLower() == item.Name.ToLower()) {
+                match = item;
+              }
+            }
+            inventory.Remove(match);
+            currentLocation.RoomContains.Add(match);
+          }
+          catch (IndexOutOfRangeException) {
+            Console.WriteLine("Please choose an item to drop.");
+          }
+        }
+
+        //Examine things more closely
+        if (splitCommands[0].ToLower() == "examine") {
+          try {
+            if (splitCommands[1].ToLower() == "room") {
+              Console.WriteLine(currentLocation.Examination);
+            } else {
+              foreach(var item in inventory) {
+                if (splitCommands[1].ToLower() == item.Name.ToLower()) {
+                  Console.WriteLine(item.Details);
+                }
+              }
+              foreach(var item in currentLocation.RoomContains) {
+                if (splitCommands[1].ToLower() == item.Name.ToLower()) {
+                  Console.WriteLine(item.Details);
+                }
+              }
+            }
+
           }
           // EXCEPTION: Item not specified
           catch (IndexOutOfRangeException) {
-            Console.WriteLine("Please choose an item to examine.");
+            Console.WriteLine("Please choose something to examine.");
           }
         }
 
--- Exercise1/Location.cs
+++ Exercise1/Location.cs
@@ -1,7 +1,29 @@
+using System.Collections.Generic;
+
 public class Location {
+  //Basic Information
   public string Name;
   public string Description;
+  public string Examination;
+
+  //Player State
   public bool JustArrived;
+
+  //Locks
+  public bool LockExists;
+  public bool NorthLocked;
+  public string NorthKey;
+  public bool SouthLocked;
+  public string SouthKey;
+  public bool EastLocked;
+  public string EastKey;
+  public bool WestLocked;
+  public string WestKey;
+
+  //Items and Interactive Objects
+  public List<Item> RoomContains;
+
+  //Mapping
   public Location North;
   public Location South;
   public Location East;
@@ -11,6 +33,7 @@ public class Location {
   public Location(string name) {
     this.Name = name;
     this.JustArrived = true;
+    this.RoomContains = new List<Item>();
   }
 
   public string GetDescription() {
@@ -19,18 +42,69 @@ public class Location {
     gameplayDescription += "*** " + this.Name + " ***\n";
     gameplayDescription += this.Description + "\n";
     if (this.North != null) {
-      gameplayDescription += "To the north you see: " + this.North.Name + "\n";
+      gameplayDescription += "To the north: " + this.North.Name + "\n";
     }
     if (this.South != null) {
-      gameplayDescription += "To the south you see: " + this.South.Name + "\n";
+      gameplayDescription += "To the south: " + this.South.Name + "\n";
     }
     if (this.East != null) {
-      gameplayDescription += "To the east you see: " + this.East.Name + "\n";
+      gameplayDescription += "To the east: " + this.East.Name + "\n";
     }
     if (this.West != null) {
-      gameplayDescription += "To the west you see: " + this.West.Name + "\n";
+      gameplayDescription += "To the west: " + this.West.Name + "\n";
     }
 
     return gameplayDescription;
+  }
+
+  //Check if accessible to player
+  public bool CheckAccess(string playerItem, string keyName) {
+    if (playerItem == keyName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //Gatekeeper
+  public string UnlockDaemon(string cardinalDirection, string selectedItem) {
+    string gameplayUnlock = "";
+    if (cardinalDirection == "north" && this.North != null && this.NorthLocked == true) {
+      if (this.CheckAccess(selectedItem, this.NorthKey)) {
+        this.NorthLocked = false;
+        this.UpdateLocks();
+        gameplayUnlock += "You unlocked the path to " + this.North.Name + " with your " + selectedItem + ".";
+      }
+    } else if (cardinalDirection == "south" && this.South != null && this.SouthLocked == true) {
+      if (this.CheckAccess(selectedItem, this.SouthKey)) {
+        this.SouthLocked = false;
+        this.UpdateLocks();
+        gameplayUnlock += "You unlocked the path to " + this.South.Name + " with your " + selectedItem + ".";
+      }
+    } else if (cardinalDirection == "east" && this.East != null && this.EastLocked == true) {
+      if (this.CheckAccess(selectedItem, this.EastKey)) {
+        this.EastLocked = false;
+        this.UpdateLocks();
+        gameplayUnlock += "You unlocked the path to " + this.East.Name + " with your " + selectedItem + ".";
+      }
+    } else if (cardinalDirection == "west" && this.West != null && this.WestLocked == true) {
+      if (this.CheckAccess(selectedItem, this.WestKey)) {
+        this.WestLocked = false;
+        this.UpdateLocks();
+        gameplayUnlock += "You unlocked the path to " + this.West.Name + " with your " + selectedItem + ".";
+      }
+    }
+    return gameplayUnlock;
+  }
+
+  //Update lock status
+  public bool UpdateLocks() {
+    if (this.NorthLocked == true || this.SouthLocked == true || this.EastLocked == true || this.WestLocked == true) {
+      this.LockExists = true;
+      return this.LockExists;
+    } else {
+      this.LockExists = false;
+      return this.LockExists;
+    }
   }
 }
